41b4584610b335f051169edf5b9388332c93a5c3
MacKubicz <kubiczk@icloud.com>
2026-01-02 19:14:16 +0100

    fix(persistence): use numpy base classes for typecheck (numpy 2.0 compat)

--- modules/reporting/persistence.py
+++ modules/reporting/persistence.py
@@ -23,11 +23,9 @@ METHOD_VERSION = RAMP_METHOD_VERSION  # Pipeline version
 class NumpyEncoder(json.JSONEncoder):
     """Custom encoder for NumPy data types."""
     def default(self, obj):
-        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
-                            np.int16, np.int32, np.int64, np.uint8,
-                            np.uint16, np.uint32, np.uint64)):
+        if isinstance(obj, np.integer):
             return int(obj)
-        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+        elif isinstance(obj, np.floating):
             return float(obj)
         elif isinstance(obj, (np.ndarray,)):
             return obj.tolist()
